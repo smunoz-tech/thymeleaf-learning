4de262f5574ba53fee35e60a5a5d5217edfec7e7
Sebastián Muñoz <cloud+github@electronmail.info>
2024-10-10 22:48:21 -0300

    Chapter 2 and 3

---
+++ book-taming-thymleaf/src/main/java/com/tamingthymeleaf/BookTamingThymleafApplication.java
@@ -0,0 +1,13 @@
+package com.tamingthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookTamingThymleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookTamingThymleafApplication.class, args);
+	}
+
+}
